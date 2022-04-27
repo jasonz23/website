1a9e51ecfc5bc73e8a609b7192b05de8f5d1e314
Jason <jason1382311@gmail.com>
2022-04-27 13:06:17 -0700

    added info for infocard

---
+++ src/Static/AboutInfo.js
@@ -0,0 +1,20 @@
+import img from "./UBC-logo.jpg"
+import tennisImg from "./tennis.jpg"
+
+const AboutInfo =[{
+    img: img,
+    title: "About Me",
+    bio: "Currently a Second Year Computer Science student at UBC. ",
+    link1: "https://www.ubc.ca/",
+    web1: ""
+
+},
+{
+    img: tennisImg,
+    title: "Interests",
+    bio: "My hobbies outside of school are tennis, video games, drawing and progrmaming. ",
+    web1: "https://jasonz23.github.io/Recipes-Project/"
+}
+]
+
+export default AboutInfo;
