1ae83343939920a80667be59cf4bbdf61a332c0f
Jason <jason1382311@gmail.com>
2022-04-27 12:39:02 -0700

    added makestyles  to card

--- src/Components/InfoCard.js
+++ src/Components/InfoCard.js
@@ -1,7 +1,28 @@
+import { CardContent, makeStyles } from "@material-ui/core";
+import React from 'react';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
 function InfoCard() {
+    const useStyles = makeStyles(() => ({
+        root: {
+            height: "100vh",
+            background: 'linear-gradient(45deg,#FF8E53 120% ,#FE6B8B 100% )'
+
+        }
+    }))
+    const classes = useStyles();
     return(
         <div>
-            Info Card
+            <Card sx = {{}}>
+                <CardContent>
+                    <h1>Cow</h1>
+                </CardContent>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                    </Typography>
+                </CardContent>
+            </Card>
         </div>
     )
 }
