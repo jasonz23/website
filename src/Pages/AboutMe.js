dbf254578b48334c8f496910673f4c8b25839954
Jason <jason1382311@gmail.com>
2022-04-27 12:39:33 -0700

    added info card to about page and styling

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,16 +1,35 @@
 import { makeStyles } from "@material-ui/core";
 import React from 'react';
-
+import InfoCard from "../Components/InfoCard";
+import Grid from '@mui/material/Grid';
 function AboutMe() {
     const useStyles = makeStyles(() => ({
         root: {
-            height: "100vh"
+            height: "100vh",
+            background: 'linear-gradient(45deg,#FF8E53 120% ,#FE6B8B 100% )',
+            display: "flex",
+            alignContent: "center",
+            justifyContent: "center"
+
+        },
+        item: {
+            width: "80vw"
         }
     }))
     const classes = useStyles();
     return(
-        <div className = {classes.root} id = "aboutme">
-            cow
+        <div className = {classes.root} id = "aboutme" >
+            <Grid container direction = "column" rowSpacing = {10} alignContent = "center" justifyContent = "center">
+                <Grid item className = {classes.item}>
+                    <InfoCard  />
+                </Grid>
+                <Grid item className = {classes.item}>
+                    <InfoCard />
+                </Grid>
+            
+                        
+            </Grid>
+            
         </div>
 
     )
