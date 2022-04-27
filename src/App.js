6d7c3a2796857ed61e5e1954631a1e0462f49f16
Jason <jason1382311@gmail.com>
2022-04-27 01:23:14 -0700

    import about me

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Header from "./Components/Header"
+import AboutMe  from "./Components/AboutMe"
 function App() {
   return (
     <div>
       <Header />
-
+      <AboutMe />
 
     </div>
   );
