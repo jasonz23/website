8122b791c3315870cbce11adc42214ae5e59116e
Jason <jason1382311@gmail.com>
2022-04-27 13:06:26 -0700

    changed styling for infocard

--- src/Components/InfoCard.js
+++ src/Components/InfoCard.js
@@ -1,8 +1,12 @@
-import { CardContent, makeStyles } from "@material-ui/core";
+import { makeStyles, Container } from "@material-ui/core";
 import React from 'react';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
-function InfoCard() {
+import Box from '@mui/material/Box';
+import UBC from "../Static/UBC-logo.jpg"
+import Grid from '@mui/material/Grid';
+
+function InfoCard(props) {
     const useStyles = makeStyles(() => ({
         root: {
             height: "100vh",
@@ -13,15 +17,37 @@ function InfoCard() {
     const classes = useStyles();
     return(
         <div>
-            <Card sx = {{}}>
-                <CardContent>
-                    <h1>Cow</h1>
-                </CardContent>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                    </Typography>
-                </CardContent>
+            <Card sx = {{
+                display: "flex",
+                flexDirection: {xs: "column", md: "row"},
+                alignItems: "center",
+                
+            }}>
+                <Box
+                component = "img"
+                sx = {{
+                    
+                    width: {xs: 1, md: 0.4},
+                    height: 0.3,
+                }}
+                alt = "UBC"
+                src = {props.info.img}
+                >
+                
+
+                </Box>
+                <Grid container display = "flex" flexDirection= " column" alignItems = "center" justifyContent = "flex-start" 
+                sx = {{
+                    p: 3
+                }}>
+                    <Grid item>
+                        <Typography variant = "h2" fontFamily= "Nunito" fontWeight = "bold"> {props.info.title}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant = "h5" fontFamily= "Nunito">{props.info.bio}</Typography>
+                    </Grid>
+                </Grid>
+
             </Card>
         </div>
     )
