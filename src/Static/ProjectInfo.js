342ccb9ad38ab1d1249d079682c0332f01ea93aa
Jason <jason1382311@gmail.com>
2022-04-27 16:31:39 -0700

    added project infomation

---
+++ src/Static/ProjectInfo.js
@@ -0,0 +1,44 @@
+import ubc from "./UBC-logo.jpg"
+const ProjectInfo =[{
+    id: 0,
+    title: "School",
+    img: ubc,
+    bio: "this is my project",
+    link: "",
+    github: "",
+    language: "template"
+}, {
+    id: 1,
+    title: "School/ Grade Manager",
+    img: ubc,
+    bio: "Application where you can manage and store classes and grades",
+    link: "https://github.com/jasonz23/School-and-Grade-Manager",
+    github: "https://github.com/jasonz23/School-and-Grade-Manager",
+},
+{
+    id:2,
+    title: "To-Do Application",
+    img: ubc,
+    bio: "A to do application built that can store and remove tasks",
+    link: "https://to-do-application-jason.netlify.app/",
+    github: "https://github.com/jasonz23/todo-application",
+},
+{
+    id: 3,
+    title: "Data Manager",
+    img: ubc,
+    bio: "A data manager than can manipulate and graph data from CSV files",
+    link: "https://github.com/jasonz23/Data-Manager",
+    github: "https://github.com/jasonz23/Data-Manager",
+},
+{
+    id: 4,
+    title: "Movie Search/Watch List",
+    img: ubc,
+    bio: "A web application that has a movie search engine and is able to store movies",
+    link: "https://moviesearchjason.netlify.app/",
+    github: "https://github.com/jasonz23/movie-search",
+}
+]
+
+export default ProjectInfo;
