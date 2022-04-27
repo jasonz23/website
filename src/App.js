343b9dd7f09d421006bc88d041524e9d286cfbb5
Jason <jason1382311@gmail.com>
2022-04-27 14:19:25 -0700

    import contact form page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from "./Pages/Header"
 import AboutMe  from "./Pages/AboutMe"
 import Project from "./Pages/Projects"
+import ContactForm from './Pages/ContactForm';
 function App() {
   return (
     <div>
       <Header />
       <AboutMe />
       <Project />
+      <ContactForm />
     </div>
   );
 }
