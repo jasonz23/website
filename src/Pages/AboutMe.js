68934a52a65b2e3aaa93cbed12f710b80dca5c2a
Jason <jason1382311@gmail.com>
2022-04-27 16:31:08 -0700

    changed margin and sizing

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -6,7 +6,7 @@ import AboutInfo from "../Static/AboutInfo";
 function AboutMe() {
     const useStyles = makeStyles(() => ({
         root: {
-            height: "110vh",
+            height: "100vh",
             background: 'linear-gradient(45deg,#FF8E53 120% ,#FE6B8B 100% )',
             display: "flex",
             alignContent: "center",
@@ -23,12 +23,14 @@ function AboutMe() {
                 <Grid item sx = {{
                     mt: {xs: 0, md: 0, lg: 0},
                     width: 0.8,
+                    maxHeight: 0.4
                     }} >
                     <InfoCard info = {AboutInfo[0]} />
                 </Grid>
                 <Grid item sx = {{  
-                    mt: {xs: 5, md: 10, lg: 10},
+                    mt: {xs: 5, md: 5, lg: 10},
                     width: 0.8,
+                    maxHeight: 0.4
                     }}>
                     <InfoCard info = {AboutInfo[1]}/>
                 </Grid>
