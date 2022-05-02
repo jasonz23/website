5af8f50c6b6efe7d7e0a0b22a3963c3d3a431144
Jason <jason1382311@gmail.com>
2022-05-02 16:59:09 -0700

    added footer

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,8 +1,32 @@
-function Footer(){
+import { makeStyles } from "@material-ui/core";
+import React from "react"
+
+function Footer() {
+    const useStyles = makeStyles(() => ({
+        root: {
+            height: "10vh",
+            background: '#524457',
+            fontFamily: "Nunito",
+            color: "white",
+            alignItems: "center",
+            justifyContent: "center",
+            display: "flex",
+            
+
+        },
+
+    }));
+    const classes = useStyles();
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className = {classes.root}>
+            
+
+            <h2>
+                Built By Jason Zhao using React
+            </h2>
+
         </div>
     )
 }
+
 export default Footer;
